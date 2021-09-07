f03748e05b641eb83926e5dd46cfb15dcdaa4edb
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-09-06 22:16:59 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,12 +1,13 @@
 from math import sqrt
 
 def fib(index):
-    if index < 2:
-        return index
-    else:
-        u = ((1+sqrt(5))/2)
-        j = ((u**index-(1-u)**index)/sqrt(5))
-        return round(j)
+    while True:
+        if index < 2:
+            return index
+        else:
+            u = ((1+sqrt(5))/2)
+            j = ((u**index-(1-u)**index)/sqrt(5))
+            return round(j)
 
 def main():
     #escribe tu código abajo de esta línea
@@ -18,3 +19,4 @@ def main():
 
 if __name__=='__main__':
     main()
+
