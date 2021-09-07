4069e828e95902d8c46e8b6f03c8f19d2b298e60
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-09-06 21:02:45 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -3,7 +3,7 @@ def main():
     #escribe tu código abajo de esta línea
     for N in range(num):
         if (N*N)>num:
-            print(min(N))
+            print(N)
     pass
 
 if __name__ == '__main__':
