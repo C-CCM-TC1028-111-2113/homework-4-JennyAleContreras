ae4fa9ddf8bac77d3d51b1e62626bbe34fec9f1c
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-09-06 23:23:58 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,14 +1,9 @@
-def Minimo(n):
-    minimo = min(n)
-    return minimo
-    
 def main():
     num = int(input("Escribe un numero : "))
     #escribe tu código abajo de esta línea
-    for n in range(0, num):
-        if (n*n)>num:
-            print(n)
-            print(Minimo(n))
+    for N in range(num):
+        if (N*N)>num:
+            print(N)
     pass
 
 if __name__ == '__main__':
